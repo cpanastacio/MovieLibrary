56748047bd482406dd74c946002f56f24edff8ec
cpanastacio <=>
2022-04-14 16:47:26 +0100

    centered movie detail page

--- frontend/src/components/MovieCard/MovieCard.js
+++ frontend/src/components/MovieCard/MovieCard.js
@@ -8,7 +8,7 @@ const MovieCard = ({ movie }) => {
 
     const onClick = (movie) => {
         const newTitle = movie.title.replaceAll(" ", "+");
-        navigate(`/tv=${newTitle}`, { state: { title: movie.title, id: movie._id } });
+        navigate(`/tv=${newTitle}`, { state: { movie: movie } });
     };
     return (
         <Card
@@ -16,7 +16,7 @@ const MovieCard = ({ movie }) => {
             style={{ width: "18rem", marginTop: "3rem", height: "43rem", marginBottom: "1rem", cursor: "pointer" }}
         >
             <Card.Img variant="top" src={movie.poster} style={{ height: "24em" }} />
-            <Card.Body>
+            <Card.Body style={{ backgroundColor: "black" }}>
                 <Card.Title>{movie.title}</Card.Title>
                 <Card.Text>Rating: {movie.rating}</Card.Text>
                 <Card.Text>
--- frontend/src/components/MovieDetails/MovieDetails.js
+++ frontend/src/components/MovieDetails/MovieDetails.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { Row, Col, Badge } from "react-bootstrap";
 
 const MovieDetails = () => {
-    const state = {
-        id: useLocation().state.id,
-        title: useLocation().state.title,
+    const { movie } = {
+        movie: useLocation().state.movie,
     };
-    return <h1>{state.title}</h1>;
+    const types = movie.genre.split(",");
+    return (
+        <Row style={{ height: "100vh" }}>
+            <Col style={{ maxWidth: "25rem", margin: "auto", marginTop: "3rem" }}>
+                <h1>{movie.title}</h1>
+                <h6>
+                    {movie.type} {movie.year}
+                </h6>
+                <img src={movie.poster} />
+                <p></p>
+                {types.map((type, i) => {
+                    return (
+                        <Badge style={{ marginRight: "1rem" }} key={i} pill bg="primary">
+                            {type}
+                        </Badge>
+                    );
+                })}
+                <p></p>
+                {movie.plot}
+            </Col>
+        </Row>
+    );
 };
 
 export default MovieDetails;
