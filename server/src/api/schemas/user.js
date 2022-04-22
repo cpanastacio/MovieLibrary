3ca66598c3fb6448fcee607849977a2c2e65a081
cpanastacio <=>
2022-04-22 04:02:39 +0100

    bcrypt and login schema added

--- server/src/api/schemas/user.js
+++ server/src/api/schemas/user.js
@@ -17,4 +17,12 @@ validationSchema.register = Joi.object({
     .required(),
 });
 
+validationSchema.login = Joi.object({
+  // eslint-disable-next-line newline-per-chained-call
+  username: Joi.string().alphanum().min(3).max(30).required(),
+  password: Joi.string()
+    .pattern(/^[a-zA-Z0-9]{3,30}$/)
+    .required(),
+});
+
 module.exports = validationSchema;
